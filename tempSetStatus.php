460fa38752d437a023e732f42cef8f02e0cf6fd6
solarbirdy <95016528+solarbirdy@users.noreply.github.com>
2021-11-25 01:35:14 -0800

    Create tempSetStatus.php
    this is... kinda awful. but it works! set your LAN's IP root or delete that part entirely to let _anyone_ set your status. which you should not do. you definitely should not do that. xD

---
+++ tempSetStatus.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+    <title>Toggle Venting and Seasons</title>
+    <link rel="STYLESHEET" type="text/css" href="css/weatherstyle.css">
+<?php // If not on the LAN, bounce home.
+if ($_SERVER['REMOTE_ADDR'] != "__YOUR_LAN_DHCP_ROOT__") {
+    print('    <meta http-equiv="refresh" content="0; URL=tempOverviewC.php">');
+    }
+?>
+
+</head>
+<body topmargin="0" style="padding-top: 0px; margin: 0px;">
+<?php
+if ( $_SERVER['REMOTE_ADDR'] == "__YOUR_LAN_DHCP_ROOT__" ) { // on the lan, welcome to panel
+    print '<span class="location">';
+    print '<style> a:link { color: maroon; font-variant: small-caps; font-style: normal; text-decoration: none; font-size: smaller; } a:visited { color: maroon; font-variant: small-caps; font-style: normal; text-decoration: none; font-size: smaller; } </style>';
+    print '<p><div style="line-height: 130%"><center>';
+    print '<a href="tempWinToggle.php/?window=east&status=closed">close</a> &nbsp; east &nbsp; ';
+    print '<a href="tempWinToggle.php/?window=east&status=open">open</a><br/>';
+//    print '<a href="tempWinToggle.php/?window=west main&status=closed">close</a> &nbsp; west main &nbsp; ';
+//    print '<a href="tempWinToggle.php/?window=west main&status=open">open</a><br/>';
+    print '<a href="tempWinToggle.php/?window=west up&status=closed">close</a> &nbsp; west up &nbsp; ';
+    print '<a href="tempWinToggle.php/?window=west up&status=open">open</a><br/>';
+//    print '<a href="tempWinToggle.php/?window=north&status=closed">close</a> &nbsp; north &nbsp; ';
+//    print '<a href="tempWinToggle.php/?window=north&status=open">open</a><br/></center></div></p>';
+    print '<p>';
+    print '<center>select mode<br/><a href="tempWinToggle.php/?window=season&status=summer">summer</a> &nbsp; ';
+    print '<a href="tempWinToggle.php/?window=season&status=winter">winter</a> &nbsp; ';
+    print '<a href="tempWinToggle.php/?window=season&status=smoke">smoke</a>';
+    print '</p></center>';
+    print '<p><center>';
+    print '<a href="tempWinToggle.php/?window=season&status=disable">disable all modes</a></center>';
+    print '</p>';
+    print '<p style="margin-top: -14px;"><center>';
+    print '<span style="font-size: medium">';
+    print '<a style="font-variant-caps: normal; font-size: medium; color: #551A8B;" href="tempOverviewC.php">Overview</a> | ';
+    print '<a style="font-variant-caps: normal; font-size: medium; color: #551A8B;" href="tempC.php">Stations</a> | ';
+    print '<a style="font-variant-caps: normal; font-size: medium; color: #551A8B;" href="tempZones.php">Zones</a> | ';
+    print '<a style="font-variant-caps: normal; font-size: medium; color: #551A8B;" href="https://ambientweather.net/dashboard/c70a0f8a27d52e86df72303460554462">Graphs</a>';
+    print '</span></center>';
+    print '</p>';
+    print '</span>';
+    }
+?>
+</body>
+</html>
