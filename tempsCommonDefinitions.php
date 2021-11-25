24b5c85be6b80b57332188319926621b102a8742
solarbirdy <95016528+solarbirdy@users.noreply.github.com>
2021-11-25 01:20:40 -0800

    Create tempsCommonDefinitions.php
    common variable definitions and a couple of defaults

---
+++ tempsCommonDefinitions.php
@@ -0,0 +1,49 @@
+<?php
+$chCelciusUnitCaps="C";
+$chCelciusUnitLowercase="c";
+$chFarenheitUnitCaps="F";
+$chFarenheitUnitLowercase="f";
+$chKelvinUnitCaps="K";
+$chKelvinUnitLowercase="k";
+$chRankineUnitCaps="R";
+$chRankineUnitLowercase="r";
+
+$chOurUnitLowercase = "BROKEN";
+$chOtherUnit1 = "BROKEN";
+$chOtherUnit2 = "BROKEN";
+
+if ($chOurUnitCaps == $chCelciusUnitCaps) {
+    $chOurUnitLowercase=$chCelciusUnitLowercase;
+    $chOtherUnit1=$chFarenheitUnitCaps;
+    $chOtherUnitPage1=$chFarenheitPage;
+    $chOtherUnit2=$chKelvinUnitCaps;
+    $chOtherUnitPage2=$chKelvinPage;
+    }
+
+if ($chOurUnitCaps == $chFarenheitUnitCaps) {
+    $chOurUnitLowercase=$chFarenheitUnitLowercase;
+    $chOtherUnit1=$chCelciusUnitCaps;
+    $chOtherUnitPage1=$chCelciusPage;
+    $chOtherUnit2=$chKelvinUnitCaps;
+    $chOtherUnitPage2=$chKelvinPage;
+    }
+
+if ($chOurUnitCaps == $chKelvinUnitCaps) {
+    $chOurUnitLowercase=$chKelvinUnitLowercase;
+    $chOtherUnit1=$chCelciusUnitCaps;
+    $chOtherUnitPage1=$chCelciusPage;
+    $chOtherUnit2=$chFarenheitUnitCaps;
+    $chOtherUnitPage2=$chFarenheitPage;
+    }
+
+if ($chOurUnitCaps == $chRankineUnitCaps) {
+    $chOurUnitLowercase=$chRankineUnitLowercase;
+    $chOtherUnit1=$chCelciusUnitCaps;
+    $chOtherUnitPage1=$chCelciusPage;
+    $chOtherUnit2=$chFarenheitUnitCaps;
+    $chOtherUnitPage2=$chFarenheitPage;
+    }
+
+$rgchOperatingSeason="summer";
+
+?>
